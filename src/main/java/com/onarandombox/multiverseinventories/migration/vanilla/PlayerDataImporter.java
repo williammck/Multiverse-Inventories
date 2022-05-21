7c7684b35b3f25314994f0bca555c20387286415
William McKinnerney <git@williammck.net>
2022-05-21 00:01:47 -0500

    Use nms/cb to parse itemstacks

--- src/main/java/com/onarandombox/multiverseinventories/migration/vanilla/PlayerDataImporter.java
+++ src/main/java/com/onarandombox/multiverseinventories/migration/vanilla/PlayerDataImporter.java
@@ -11,20 +11,17 @@ import com.onarandombox.multiverseinventories.profile.ProfileTypes;
 import com.onarandombox.multiverseinventories.profile.container.ContainerType;
 import com.onarandombox.multiverseinventories.share.Sharables;
 import com.onarandombox.multiverseinventories.util.MinecraftTools;
-import net.minidev.json.JSONObject;
-import net.minidev.json.JSONValue;
-import org.bukkit.ChatColor;
+import net.minecraft.server.v1_16_R3.NBTCompressedStreamTools;
+import net.minecraft.server.v1_16_R3.NBTTagCompound;
+import net.minecraft.server.v1_16_R3.NBTTagList;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
-import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.craftbukkit.v1_16_R3.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -45,139 +42,7 @@ public class PlayerDataImporter {
         return new File(this.plugin.getServer().getWorldContainer() + File.separator + this.world + File.separator + "playerdata");
     }
 
-    private String parseLabel(JSONObject json, boolean isLore) {
-        StringBuilder finalString = new StringBuilder();
-
-        // by default, custom text is italic; check if we need to remove it
-        if (json.containsKey("italic") && !(boolean) json.get("italic")) {
-            finalString.append(ChatColor.RESET);
-            // by default, lore is dark_purple, so we might need to set it again since we reset
-            if (isLore && !json.containsKey("color")) finalString.append(ChatColor.DARK_PURPLE);
-        }
-
-        if (json.containsKey("bold") && (boolean) json.get("bold")) finalString.append(ChatColor.BOLD);
-        if (json.containsKey("obfuscated") && (boolean) json.get("obfuscated")) finalString.append(ChatColor.MAGIC);
-        if (json.containsKey("strikethrough") && (boolean) json.get("strikethrough")) finalString.append(ChatColor.STRIKETHROUGH);
-        if (json.containsKey("underlined") && (boolean) json.get("underlined")) finalString.append(ChatColor.UNDERLINE);
-        if (json.containsKey("color")) finalString.append(ChatColor.valueOf((String) json.get("color")));
-        if (json.containsKey("text")) finalString.append((String) json.get("text"));
-
-        return finalString.toString();
-    }
-
-    private void parseItem(ItemStack item, Tag[] tags) {
-        ItemMeta itemMeta = item.getItemMeta();
-
-        for (Tag tag: tags) {
-            if (tag.getName() == null) continue;
-            switch (tag.getName()) {
-                // these are generic
-                case "CanDestroy":
-                    // TODO
-                    break;
-                case "CustomModelData":
-                    // TODO
-                    break;
-                case "Damage":
-                    if (itemMeta instanceof Damageable) {
-                        Damageable d = (Damageable) itemMeta;
-                        d.setDamage((int) tag.getValue());
-                        itemMeta = (ItemMeta) d;
-                    }
-                    break;
-                case "Unbreakable":
-                    itemMeta.setUnbreakable((int) tag.getValue() == 1);
-                    break;
-                // these are only for blocks
-                case "BlockEntityTag":
-                    // TODO
-                    break;
-                case "BlockStateTag":
-                    // TODO
-                    break;
-                case "CanPlaceOn":
-                    // TODO
-                    break;
-                // these have to do with enchantments
-                case "Enchantments":
-                    // TODO
-                    break;
-                case "RepairCost":
-                    // TODO
-                    break;
-                case "StoredEnchantments":
-                    // TODO
-                    break;
-                // these apply to all items, except for "color" in "display" which is only for leather armor
-                case "AttributeModifiers":
-                    // TODO
-                    break;
-                case "display":
-                    for (Tag displayTag: (Tag[]) tag.getValue()) {
-                        if (displayTag.getName() == null) continue;
-                        switch (displayTag.getName()) {
-                            case "color":
-                                // TODO
-                                break;
-                            case "Lore":
-                                // TODO: take care of "extra" lore like clickEvent and hoverEvent
-                                ArrayList<String> lore = new ArrayList<>();
-                                for (Tag loreTag: (Tag[]) displayTag.getValue()) {
-                                    JSONObject json = (JSONObject) JSONValue.parse((String) loreTag.getValue());
-                                    lore.add(parseLabel(json, true));
-                                }
-                                itemMeta.setLore(lore);
-                                break;
-                            case "Name":
-                                JSONObject json = (JSONObject) JSONValue.parse((String) displayTag.getValue());
-                                itemMeta.setDisplayName(parseLabel(json, false));
-                                break;
-                        }
-                    }
-                    break;
-                case "HideFlags":
-                    // TODO
-                    break;
-                // these are for potion effects
-                case "CustomPotionColor":
-                    // TODO
-                    break;
-                case "CustomPotionEffects":
-                    // TODO
-                    break;
-                case "Potion":
-                    // TODO
-                    break;
-                // these are only for crossbows
-                case "Charged":
-                    // TODO
-                    break;
-                case "ChargedProjectiles":
-                    // TODO
-                    break;
-                // the following are only for books
-                case "author":
-                    // TODO
-                    break;
-                case "generation":
-                    // TODO
-                    break;
-                case "pages":
-                    // TODO
-                    break;
-                case "resolved":
-                    // TODO
-                    break;
-                case "title":
-                    // TODO
-                    break;
-            } // TODO: Player Heads, Fireworks, Armor Stands and Spawn Eggs, Buckets of Fish, Maps, Suspicious Stew, Debug Sticks, Compasses
-        }
-
-        item.setItemMeta(itemMeta);
-    }
-
-    private void parseInventoryItems(Tag[] items, ItemStack[][] stacks, int size) {
+    private void parseInventoryItems(NBTTagList inventory, ItemStack[][] stacks, int size) {
         // this is the main inventory
         stacks[0] = MinecraftTools.fillWithAir(new ItemStack[size]);
         // this is the armor contents
@@ -185,11 +50,12 @@ public class PlayerDataImporter {
         // this is the off hand item
         stacks[2] = MinecraftTools.fillWithAir(new ItemStack[1]);
 
-        if (items != null) {
-            for (Tag item: items) {
+        for (int i = 0; i < inventory.size() - 1; i++) {
+            NBTTagCompound compound = (NBTTagCompound) inventory.get(i);
+            if (!compound.isEmpty()) {
                 byte inv;
                 // this will not cause an NPE (it always appears in playerdata)
-                byte slot = (byte) item.findTagByName("Slot").getValue();
+                byte slot = compound.getByte("Slot");
 
                 // we need to check this since armor and off hand item are also stored in the inventory
                 if (-1 < slot && slot < 36) {
@@ -203,15 +69,8 @@ public class PlayerDataImporter {
                 } else inv = -1;
 
                 if (inv != -1) {
-                    // these will not cause NPEs (they always appear in playerdata)
-                    String id = (String) item.findTagByName("id").getValue();
-                    byte count = (byte) item.findTagByName("Count").getValue();
-                    stacks[inv][slot] = new ItemStack(Material.matchMaterial(id), count);
-
-                    // this tag is not always present in playerdata! be mindful of NPEs!
-                    Tag tag = item.findTagByName("tag");
-                    // if tag exists, this item has some special data we need to read
-                    if (tag != null) parseItem(stacks[inv][slot], (Tag[]) tag.getValue());
+                    ItemStack stack = CraftItemStack.asBukkitCopy(net.minecraft.server.v1_16_R3.ItemStack.a(compound));
+                    stacks[inv][slot] = stack;
                 }
             }
         }
@@ -223,6 +82,7 @@ public class PlayerDataImporter {
         for (File playerData: playerDataFolder.listFiles()) {
             try {
                 Tag nbt = Tag.readFrom(new FileInputStream(playerData));
+                NBTTagCompound nmsNbt = NBTCompressedStreamTools.a(new FileInputStream(playerData));
 
                 int dataVersion;
                 Tag dataVersionTag = nbt.findTagByName("DataVersion");
@@ -272,7 +132,8 @@ public class PlayerDataImporter {
                             break;
                         case "EnderItems":
                             ItemStack[][] enderItems = new ItemStack[3][];
-                            parseInventoryItems((Tag[]) tag.getValue(), enderItems, PlayerStats.ENDER_CHEST_SIZE);
+                            NBTTagList enderInv = (NBTTagList) nmsNbt.get("EnderItems");
+                            parseInventoryItems(enderInv, enderItems, PlayerStats.ENDER_CHEST_SIZE);
                             pp.set(Sharables.ENDER_CHEST, enderItems[0]);
                             break;
                         case "FallDistance":
@@ -295,7 +156,8 @@ public class PlayerDataImporter {
                             break;
                         case "Inventory":
                             ItemStack[][] stacks = new ItemStack[3][];
-                            parseInventoryItems((Tag[]) tag.getValue(), stacks, PlayerStats.INVENTORY_SIZE);
+                            NBTTagList inventory = (NBTTagList) nmsNbt.get("Inventory");
+                            parseInventoryItems(inventory, stacks, PlayerStats.INVENTORY_SIZE);
                             pp.set(Sharables.INVENTORY, stacks[0]);
                             pp.set(Sharables.ARMOR, stacks[1]);
                             pp.set(Sharables.OFF_HAND, stacks[2][0]);
